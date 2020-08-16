bbfec6082a9cd1baf19e7a2fb2656321f786b3d7
sohaila-elbasel <sohailaelbasel@hotmail.com>
2020-08-16 21:20:26 +0200

    Finish the project v1.0

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort
 from forms import Addshow
-from imports import read_file, write_file
+from imports import read_file, write_file, search, delete
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e14b0006b64d6f97042d224d38050df3'
@@ -34,9 +34,32 @@ def add():
             return redirect(url_for('home'))
     return render_template('add.html', form = form)
 
-@app.route('/edit')
-def edit():
-    return render_template('edit.html')
+@app.route('/edit/<string:name>', methods=['GET', 'POST'])
+def edit(name):
+    show = search(name)
+    form = Addshow()
+    if request.method == 'GET':
+        form.name.data = show['name']
+        form.type.data = show['type']
+        form.season.data = show['season']
+        form.last_ep.data = show['last_ep']
+        return render_template('edit.html', form = form, show=show)
+    else:
+        if request.form['action'] == 'Edit':
+            if form.validate_on_submit():
+                delete(form.name.data)
+                show = form.name.data + ',' + form.type.data + ','+ form.season.data + ',' + form.last_ep.data
+                write_file(show)
+                return redirect(url_for('home'))
+        elif request.form['action'] == 'Delete':
+            delete(form.name.data)
+            return redirect(url_for('home'))
+        else:
+            abort(404)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
--- imports.py
+++ imports.py
@@ -1,4 +1,14 @@
-def read_file(file='list.txt'):
+file='list.txt'
+def make_dict(line):
+    show = {}
+    show['name'] = line[0]
+    show['type'] = line[1]
+    show['season'] = line[2]
+    show['last_ep'] = line[3]
+    return show
+
+
+def read_file(file = file):
     list = []
     try:
         with open(file, 'r') as f:
@@ -7,17 +17,30 @@ def read_file(file='list.txt'):
             content = [x.replace('\r', "") for x in content]
             content = [x.split(',') for x in content]
         for line in content:
-            show = {}
-            show['name'] = line[0]
-            show['type'] = line[1]
-            show['season'] = line[2]
-            show['last_ep'] = line[3]
-            list.append(show)
+            list.append(make_dict(line))
 
         return list
     except:
         raise('The file is empty')
 
-def write_file(show, file='list.txt'):
+def write_file(show, file = file):
     with open(file, 'a') as f:
         f.write(f'{show}\n')
+
+def search(name, file = file):
+    with open(file, 'r') as f:
+        content = f.readlines()
+        content = [x.replace('\n', "") for x in content]
+        content = [x.replace('\r', "") for x in content]
+        content = [x.split(',') for x in content]
+    for line in content:
+        if name in line:
+            return make_dict(line)
+
+def delete(name, file = file):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    with open(file, 'w') as f:
+        for line in lines:
+            if name not in line.split(',')[0]:
+                f.write(line)
